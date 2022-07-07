40f0342164983a4fb8126842ba4f17a01e564faa
Zeeshan Ahmed <zeeshan.ahmed@invozone.com>
2022-07-08 03:06:47 +0500

    missing keys added to plist and workflows modified

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -3,6 +3,6 @@
 set -eo pipefail
 
 xcodebuild -workspace OTFMagicBox.xcworkspace \
-            -scheme OTFMagicBox
+            -scheme OTFMagicBox \
             -destination platform=iOS\ Simulator,OS=15.5,name=iPhone\ 13 \
             clean test | xcpretty
