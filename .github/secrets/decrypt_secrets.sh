95376bf2cc89895c5265645c74764e4ccff04b0a
Zeeshan Ahmed <zeeshan.ahmed@invozone.com>
2022-07-08 02:25:23 +0500

    secrets names' spellings corrected

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -4,19 +4,14 @@
 
 set -euo pipefail
 
-security create-keychain -p "${SIGINING_CERTIFICATES_PASSWORD}" build.keychain
+security create-keychain -p "" build.keychain
 security list-keychains -s build.keychain
 security default-keychain -s build.keychain
-security unlock-keychain -p "${SIGINING_CERTIFICATES_PASSWORD}" build.keychain
+security unlock-keychain -p "" build.keychain
 security set-keychain-settings
-security import <(echo "${SIGINING_CERTIFICATES_DATA_DEBUG}" | base64 --decode) \
+security import <(echo "${SIGNING_CERTIFICATE_DATA_DEBUG}" | base64 --decode) \
                 -f pkcs12 \
                 -k build.keychain \
-                -P "${SIGINING_CERTIFICATES_PASSWORD}" \
+                -P "${SIGNING_CERTIFICATES_PASSWORD}" \
                 -T /usr/bin/codesign
-security import <(echo "${SIGINING_CERTIFICATES_DATA_RELEASE}" | base64 --decode) \
-                -f pkcs12 \
-                -k build.keychain \
-                -P "${SIGINING_CERTIFICATES_PASSWORD}" \
-                -T /usr/bin/codesign
-security set-key-partition-list -S apple-tool:,apple: -s -k "${SIGINING_CERTIFICATES_PASSWORD}" build.keychain
+security set-key-partition-list -S apple-tool:,apple: -s -k "" build.keychain
